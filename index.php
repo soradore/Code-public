f1d8b171623ffba325a53a5e0b9f99617b55d0d7
soradore <soradore@svk.jp>
2018-09-06 23:40:19 +0900

    add and remove files

--- index.php
+++ index.php
@@ -5,13 +5,8 @@ require "db.php";
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <!-- <link type="text/css" rel="stylesheet" href="./syntaxhighlighter/styles/shCore.css" />
-    <link type="text/css" rel="stylesheet" href="./syntaxhighlighter/styles/shThemeDefault.css" />-->
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!--<script src="./syntaxhighlighter/scripts/shCore.js"></script>
-    <script type="text/javascript" src="./syntaxhighlighter/scripts/shBrushPhp.js"></script>
-    <script type="text/javascript" src="./syntaxhighlighter/scripts/shBrushJava.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js"></script>
     <style type="text/css">
@@ -57,9 +52,6 @@ $code = $data["code"];
  <input type="hidden" id="id" value="<?=$id?>">
  <input type="password" id="pass" placeholder="password">
  <button type="button" id="delete">削除</button>
- <!-- <script>
-     $('#code').prepend('<pre class="brush: <?=$type?>">' + '<?=$code?>' + '</pre>');
- </script> -->
 
 
 
@@ -68,9 +60,6 @@ $code = $data["code"];
     $("#delete").click(function(){
         var pass = $("#pass").val();
         var id = $("#id").val();
-
-        //alert(pass + " / " + id);
-        //return 0;
         pass = pass.replace(/\s+/g, "");
         if(pass == ""){
             alert("パスワードが未記入です");
@@ -113,11 +102,8 @@ $code = $data["code"];
  </script>
 </body>
 <?php else : ?>
-
 <head>
     <title>コード共有</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js"></script>
 </head>
 <body>
  
@@ -183,8 +169,6 @@ $code = $data["code"];
             var type = $("[name=type]").val();
             var title = $("#title").val();
             var code = editor.getValue();
-            //code = nl2br(code);
-            //code = escape_html(code);
             if(code == ""){
                 alert("コードが空です"); 
                 return false;
