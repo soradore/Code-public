240a5798039193242d0987cdf7a55bdaf58ea4b9
soradore <soradore@svk.jp>
2018-09-26 23:24:16 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -5,8 +5,13 @@ require "db.php";
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- <link type="text/css" rel="stylesheet" href="./syntaxhighlighter/styles/shCore.css" />
+    <link type="text/css" rel="stylesheet" href="./syntaxhighlighter/styles/shThemeDefault.css" />-->
+
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!--<script src="./syntaxhighlighter/scripts/shCore.js"></script>
+    <script type="text/javascript" src="./syntaxhighlighter/scripts/shBrushPhp.js"></script>
+    <script type="text/javascript" src="./syntaxhighlighter/scripts/shBrushJava.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js"></script>
     <style type="text/css">
@@ -44,7 +49,7 @@ $code = $data["code"];
     });
     editor.$blockScrolling = Infinity;
     editor.setTheme("ace/theme/monokai");
-    editor.setFontSize(14);
+    editor.setFontSize(10);
     editor.getSession().setMode("ace/mode/<?=$type?>");
     editor.getSession().setTabSize(4);
  </script>
@@ -60,6 +65,9 @@ $code = $data["code"];
     $("#delete").click(function(){
         var pass = $("#pass").val();
         var id = $("#id").val();
+
+        //alert(pass + " / " + id);
+        //return 0;
         pass = pass.replace(/\s+/g, "");
         if(pass == ""){
             alert("パスワードが未記入です");
@@ -85,6 +93,7 @@ $code = $data["code"];
             switch(res){
                 case 'success':
                     alert("削除しました");
+                    //location.replace("http://code.pocketmp.xyz");
                     break;
                 case 'pass_faild':
                     alert("パスワードが正しくありません");
@@ -97,11 +106,15 @@ $code = $data["code"];
         });
 
     });
+    -->
  </script>
 </body>
 <?php else : ?>
+
 <head>
     <title>コード共有</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js"></script>
 </head>
 <body>
  
@@ -111,8 +124,9 @@ $code = $data["code"];
     <option value="php">PHP</option>
     <option value="java">JAVA</option>
  </select>
- <div id="info">
-  <input type="text" id="title" value="exmple!!"><button type="button" id="save">保存</button>
+ <div id="info"></div>
+ <div id="input">
+  <input type="text" id="title" placeholder="title"><button type="button" id="save">保存</button>
  </div>
  <div id="code" style="height: 500px; width: 80%"></div>
  <script type="text/javascript">
@@ -139,30 +153,14 @@ $code = $data["code"];
  </script>
 
  <script type="text/javascript">
-   function escape_html (string) {
-      if(typeof string !== 'string') {
-        return string;
-    }
-    return string.replace(/[&'`"<>]/g, function(match) {
-        return {
-          '&': '&amp;',
-          "'": '&#x27;',
-          '`': '&#x60;',
-          '"': '&quot;',
-          '<': '&lt;',
-          '>': '&gt;',
-            }[match]
-        });
-    }
- </script>
-
- <script type="text/javascript">
     $(function(){
 
         $("#save").click(function(){
             var type = $("[name=type]").val();
             var title = $("#title").val();
             var code = editor.getValue();
+            //code = nl2br(code);
+            //code = escape_html(code);
             if(code == ""){
                 alert("コードが空です"); 
                 return false;
@@ -181,6 +179,7 @@ $code = $data["code"];
                 var pass = res.pass;
                 var id = res.id;
                 $("#info").html("<p><font color='green'>保存しました</font></p><p><a href=http://localhost:8080/?id=" + id + "> コード </a>password: <input type='text' value=" + pass + " readonly></p>");
+                $("#input").html("");
                 
             }).fail(function(XMLHttpRequest, textStatus, errorThrown){
                 alert(errorThrown);
